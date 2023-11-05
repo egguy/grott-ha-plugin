d74ce078e16d1f577868c647ecd4420999cbf41e
egguy <etienne.guilluy@gmail.com>
2023-11-06 00:28:27 +1100

    Add missing removed files

--- src/grott/extension/ha/interface.py
+++
@@ -1,7 +0,0 @@
-# Used to simulate a Conf object from grott
-class FakeConf:
-    def __init__(self):
-        self.recorddict = {}
-        self.layout = ""
-        self.extvar = {}
-        self.verbose = True
