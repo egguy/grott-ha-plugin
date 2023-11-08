c68f323b0c55868214103dd326e64fe3f3bae01c
egguy <egguy@users.noreply.github.com>
2023-11-08 12:35:54 +0000

    deploy: 723c95d5b358728313902fa98ddd9c7d99ef0652

--- src/grottext/ha/__init__.py
+++
@@ -1,4 +0,0 @@
-from grottext.ha.__version__ import __version__
-from grottext.ha.extension import grottext
-
-__all__ = ["__version__", "grottext"]
--- src/grottext/ha/constants.py
+++
@@ -1,9 +0,0 @@
-CONFIG_TOPIC = "homeassistant/{sensor_type}/grott/{device}_{attribut}/config"
-STATE_TOPIC = "homeassistant/grott/{device}/state"
-
-
-MQTT_HOST_CONF_KEY = "ha_mqtt_host"
-MQTT_PORT_CONF_KEY = "ha_mqtt_port"
-MQTT_USERNAME_CONF_KEY = "ha_mqtt_user"
-MQTT_PASSWORD_CONF_KEY = "ha_mqtt_password"
-MQTT_RETAIN_CONF_KEY = "ha_mqtt_retain"
